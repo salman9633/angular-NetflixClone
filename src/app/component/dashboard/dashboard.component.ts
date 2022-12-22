d838ac10a1d6be9567697b6a327cae44504c255b
salmanfaris <salmanvaipees913@gmail.com>
2022-12-22 10:17:29 +0530

    env

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../Model/movie'
 import { DataService } from 'src/app/service/data.service';
 import { environment } from 'src/environments/environment';
@@ -9,9 +9,9 @@ import { environment } from 'src/environments/environment';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.getLatestMovie();
     this.getPopularMovies();
     this.getNowPlayingMovies();
@@ -21,7 +21,7 @@ export class DashboardComponent implements OnInit{
     this.getOriginals();
   }
 
-  latestMovie : any;
+  latestMovie: any;
   popularMovies !: Movie;
   nowPlayingMovies !: Movie;
   topRatedMovies !: Movie;
@@ -30,88 +30,98 @@ export class DashboardComponent implements OnInit{
   originals !: Movie;
 
 
-  constructor(private dataService:DataService){
+  constructor(private dataService: DataService) {
 
   }
 
 
 
-  getLatestMovie(){
-    this.dataService.getLatestMovie().subscribe((res: any)=>{
-      this.latestMovie=this.changeData(res);
-      console.log(this.latestMovie,'llllllllllll');
-      
-    },(err: any)=>{
-      console.log("NO LATEST",err);
-      
+  getLatestMovie() {
+    this.dataService.getLatestMovie().subscribe((res: any) => {
+      console.log(res, 'ppppppppppppppp');
+
+      this.latestMovie = this.changeData(res);
+      console.log(this.latestMovie, 'llllllllllll');
+
+    }, (err: any) => {
+      console.log("NO LATEST", err);
+
     })
   }
   changeData(res: any): any {
-    if(!res.backdrop_path){
-      res.backdrop_path='https://image.tmbd.org/t/p/original' + res.poster_path + '?api_key=' + environment.api_key;
-    }else{
-      res.backdrop_path='https://image.tmbd.org/t/p/original' + res.backdrop_path + '?api_key=' + environment.api_key;
+    if (!res.backdrop_path) {
+      res.backdrop_path = 'https://image.tmbd.org/t/p/original' + res.poster_path + '?api_key=' + environment.api_key;
+    } else {
+      res.backdrop_path = 'https://image.tmbd.org/t/p/original' + res.backdrop_path + '?api_key=' + environment.api_key;
     }
+    console.log(res, 'yyyy');
+
     return res;
   }
-  getPopularMovies(){
-    this.dataService.getPopularMovies().subscribe((res: any)=>{
-      this.popularMovies=this.modifyData(res);
-    },(err: any)=>{
-      console.log("NO Popular",err);
-      
-    })
-  }
-  getNowPlayingMovies(){
-    this.dataService.getNowPlayingMovies().subscribe((res: any)=>{
-      this.nowPlayingMovies=this.modifyData(res);
-    },(err: any)=>{
-      console.log("NO NOWPLAYING",err);
-      
-    })
-  }
-  getTopRatedMovies(){
-    this.dataService.getTopRatedMovies().subscribe((res: any)=>{
-      this.topRatedMovies=this.modifyData(res);
-    },(err: any)=>{
-      console.log("NO topRatedMovies",err);
-      
-    })
-  }
-  getUpcomingMovies(){
-    this.dataService.getUpcomingMovies().subscribe((res: any)=>{
-      this.upComingMovies=this.modifyData(res);
-    },(err: any)=>{
-      console.log("NO upComingMovies",err);
-      
-    })
-  }
-  getTrendingMovies(){
-    this.dataService.getTrendingMovies().subscribe((res: any)=>{
-      this.trendingMovies=this.modifyData(res);
-    },(err: any)=>{
-      console.log("NO trendingMovies",err);
-      
-    })
-  }
-  getOriginals(){
-    this.dataService.getOriginals().subscribe((res: any)=>{
-      this.originals=this.modifyData(res);
-    },(err: any)=>{
-      console.log("NO trendingMovies",err);
-      
+
+  getPopularMovies() {
+    this.dataService.getPopularMovies().subscribe((res: any) => {
+      this.popularMovies = this.modifyData(res);
+    }, (err: any) => {
+      console.log("NO Popular", err);
+
     })
   }
 
-  modifyData(movies:Movie): Movie{
-    if(movies.results){
-      movies.results.forEach(element=>{
-        element.backdrop_path='https://image.tmbd.org/t/p/original'+element.backdrop_path+'?api_key='+environment.api_key;
-        if(!element.title){
-          element.title=element?.name;
+  getNowPlayingMovies() {
+    this.dataService.getNowPlayingMovies().subscribe((res: any) => {
+      this.nowPlayingMovies = this.modifyData(res);
+    }, (err: any) => {
+      console.log("NO NOWPLAYING", err);
+
+    })
+  }
+  getTopRatedMovies() {
+    this.dataService.getTopRatedMovies().subscribe((res: any) => {
+      this.topRatedMovies = this.modifyData(res);
+    }, (err: any) => {
+      console.log("NO topRatedMovies", err);
+
+    })
+  }
+  getUpcomingMovies() {
+    this.dataService.getUpcomingMovies().subscribe((res: any) => {
+      this.upComingMovies = this.modifyData(res);
+    }, (err: any) => {
+      console.log("NO upComingMovies", err);
+
+    })
+  }
+  getTrendingMovies() {
+    this.dataService.getTrendingMovies().subscribe((res: Movie) => {
+      console.log(res, 'eeeeeeeeeeeeeeee');
+
+      this.trendingMovies = this.modifyData(res);
+    }, (err: any) => {
+      console.log("NO trendingMovies", err);
+
+    })
+  }
+  getOriginals() {
+    this.dataService.getOriginals().subscribe((res: any) => {
+      this.originals = this.modifyData(res);
+    }, (err: any) => {
+      console.log("NO trendingMovies", err);
+
+    })
+  }
+
+  modifyData(movies: Movie): Movie {
+    if (movies.results) {
+      movies.results.forEach(element => {
+        element.backdrop_path = 'https://image.tmbd.org/t/p/original' + element.backdrop_path + '?api_key=' + environment.api_key;
+        if (!element.title) {
+          element.title = element?.name;
         }
       })
     }
-    return movies
+    console.log(movies,'movies......................');
+    
+    return movies;
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment={
+    production:false,
+    api_key:'0da97fd8579f9fd887b8830022a7b39e'
+}
